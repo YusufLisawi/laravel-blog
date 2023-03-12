f60d883fdf66b468736a20ec71ef7e56a616105c
YusufLisawi <yusufisawi@gmail.com>
2023-03-12 14:28:45 +0100

    Updates

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,9 +5,9 @@
                 {{ __('Users') }}
             </h2>
             <button onclick="Livewire.emit('openModal', 'add-user')"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150ƒ">Create
+                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Create
                 user</button>
-        </div>
+      </div>
     </x-slot>
 
     <div class="py-12">
